639e247581ec1e0c3270d6e4fe9bc2cb05c81434
Nick Edwards <nledwa01@gmail.com>
2025-11-27 15:17:05 -0600

    Updated svg logo. Added Nav section and Hero section placeholders. Added Hero section image.

--- figma-landing-page/src/App.jsx
+++ figma-landing-page/src/App.jsx
@@ -3,8 +3,24 @@ import './App.css';
 function App() {
     return (
         <>
-            <p className='text-5xl font-bold'>Hello world!</p>
+            <NavBar />
+            <Hero />
         </>
+    );
+}
+
+function NavBar() {
+    return <div>Nav Section</div>;
+}
+
+function Hero() {
+    return (
+        <div className='flex justify-between items-center m-4'>
+            <h1 className='text-5xl font-bold'>Hero Section</h1>
+            <div className='m-4'>
+                <img src='src/assets/HeroShot-min.jpg' alt='FigmaSample' />
+            </div>
+        </div>
     );
 }
 

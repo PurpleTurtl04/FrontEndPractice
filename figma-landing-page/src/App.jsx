7061a5a4a480c28e51aba3292adbea496d2fa66f
Nick Edwards <nledwa01@gmail.com>
2025-12-01 16:47:17 -0600

    Completed Collaborate section. Added Sign Up form section

--- figma-landing-page/src/App.jsx
+++ figma-landing-page/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
     return (
-        <div className="container max-w-full bg-black">
+        <div className="container max-w-full bg-black text-white">
             <header>
                 <NavBar />
             </header>
@@ -10,6 +10,7 @@ function App() {
                 <Hero />
                 <Brands />
                 <Collaborate />
+                <SignUpForm />
             </main>
         </div>
     );
@@ -40,14 +41,14 @@ function Hero() {
         <section className="mb-48">
             <div className="flex items-center justify-between gap-10">
                 <div className="w-1/3">
-                    <h1 className="text-5xl font-bold text-white">
+                    <h1 className="text-5xl font-bold">
                         A better way to design.
                     </h1>
                     <p className="mt-13 text-gray-400">
                         Design, prototype, and collaborate all in the
                         browser—with Figma.
                     </p>
-                    <button className="mt-8 rounded-sm bg-green-400 px-10 py-3 text-white">
+                    <button className="mt-8 rounded-sm bg-green-400 px-10 py-3">
                         Try Figma for free
                     </button>
                 </div>
@@ -62,9 +63,7 @@ function Hero() {
 function Brands() {
     return (
         <section className="mb-48">
-            <p className="mb-20 text-center text-white">
-                Empowering innovative brands
-            </p>
+            <p className="mb-20 text-center">Empowering innovative brands</p>
             <div className="mx-auto flex max-w-3xl flex-wrap justify-between gap-x-19 gap-y-13">
                 <img
                     className="size-15"
@@ -133,14 +132,88 @@ function Brands() {
 
 function Collaborate() {
     return (
-        <section className="text-center">
-            <h3 className="mb-7 text-xl text-white">
-                Collaborate with everyone, at every phase
-            </h3>
-            <p className="max-w-lg text-white">
-                Each contributor owns their part of the creative process and
-                stays in sync along the way—across any platform.
-            </p>
+        <section className="mb-15">
+            <div className="mb-22 flex flex-col items-center text-center">
+                <h3 className="mb-7 text-xl">
+                    Collaborate with everyone, at every phase
+                </h3>
+                <p className="max-w-lg">
+                    Each contributor owns their part of the creative process and
+                    stays in sync along the way—across any platform.
+                </p>
+            </div>
+            <Collaborators />
+            <div className="text-center">
+                <button className="mx-auto rounded-sm bg-green-400 px-5.75 py-3 text-white">
+                    Try Figma for free
+                </button>
+            </div>
+        </section>
+    );
+}
+
+function Collaborators() {
+    return (
+        <div className="mb-10 flex justify-between gap-8">
+            <div className="flex flex-col">
+                <img
+                    src="/src/assets/designers_image-min.jpg"
+                    alt="Designer Example"
+                    className="mb-9 size-80"
+                />
+                <h3 className="mb-4 pl-px text-xl">Your designers</h3>
+                <p className="pl-px">
+                    Collaborate in context and in real time. Never worry about
+                    your files being out of date or overwriting each other’s
+                    work.
+                </p>
+            </div>
+            <div>
+                <img
+                    src="/src/assets/stakeholders_image-min.jpg"
+                    alt="Stakeholder Example"
+                    className="mb-9 size-80"
+                />
+                <h3 className="mb-4 pl-px text-xl">Your stakeholders</h3>
+                <p className="pl-px">
+                    Engineers always have access to the current source-of-truth
+                    and can inspect elements, export assets, and copy code.
+                </p>
+            </div>
+            <div>
+                <img
+                    src="/src/assets/developers_image-min.jpg"
+                    alt="Developer Example"
+                    className="mb-9 size-80"
+                />
+                <h3 className="mb-4 pl-px text-xl">Your developers</h3>
+                <p className="pl-px">
+                    Engineers always have access to the current source-of-truth
+                    and can inspect elements, export assets, and copy code.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+function SignUpForm() {
+    return (
+        <section className="flex w-lvw flex-col items-center bg-white pt-31 text-center text-black">
+            <p className="mb-11 font-semibold">Try Figma for Free</p>
+            <button className="mb-5 flex w-sm items-center justify-center rounded-sm border border-solid border-gray-300 px-25 py-4">
+                <img src="/src/assets/Google.svg" alt="Google Logo" /> Sign up
+                with Google
+            </button>
+            <p className="mb-5 text-gray-300">or</p>
+            <button className="mb-5 flex w-sm rounded-sm border border-solid border-gray-300 py-4 pl-5 text-gray-300">
+                Email
+            </button>
+            <button className="mb-5 flex w-sm rounded-sm border border-solid border-gray-300 py-4 pl-5 text-gray-300">
+                Password
+            </button>
+            <button className="mb-5 w-sm rounded-sm border border-solid bg-green-400 py-4 pl-5 text-center text-white">
+                Create account...
+            </button>
         </section>
     );
 }
